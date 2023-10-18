770e943d84e5e90a59a34a46591150c8aef186bd
Daniel Garner <147892775+DanielFGarner@users.noreply.github.com>
2023-10-18 15:30:55 -0400

    Update timeQuiz.py

--- timeQuiz.py
+++ timeQuiz.py
@@ -7,7 +7,7 @@ while userTime != '0':
     minute = randint(1,60)
     militaryTime = f'{hour:0>2}{minute:0>2}'
 
-    print(f"{hour - 12 if hour > 12 else hour}:{minute:0>2}{'am' if hour <= 12 else 'pm'}")
+    print(f"{hour - 12 if hour >= 12 else hour}:{minute:0>2}{'am' if hour <= 12 else 'pm'}")
 
     correct = False
     while correct is False:
